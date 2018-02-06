66e88ce747d7da086ae6ca36ff65c3ede5b06eb9
malgabre97 <36145433+malgabre97@users.noreply.github.com>
2018-02-06 08:36:51 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+<head><title>Web Dev</title></head>
+<style>
+div{
+background-color:purple;
+}
+</style>
+<div>
+<h1><b>My Information</b></h1>
+</div>
+<body>
+<p><h5><b>Name:</b> Melfer P. Algabre</h5></p>
+<p><h5><b>Address:</b> Balirong, City of Naga Cebu</h5></p>
+<p><h5><b>Age:</b> 20</h5></p>
+<p><h5><b>Date of Birth:</b> April 23, 1997</h5></p>
+<p><h5><b>Place of Birth:</b> Punod, Balirong, City of Naga Cebu</h5></p>
+<p><h5><b>Gender:</b> Female</h5></p>
+<p><h5><b>Religion:</b> Roman Catholic</h5></p>
+<p><h5><b>Nationality:</b> Filipino</h5></p>
+<p><h5><b>Hobbies:</b> Singing and Dancing, Reading and Writing</h5></p>
+<div>
+<h1><b>Educational Attainment</b></h1>
+</div>
+<p><h5><b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbspName of School:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A.Y.:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Awards Received:</b></h5></p>
+<p><h5>Tagjaguimit Elementary School&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2009&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Service Awardee</h5></p>
+<p><h5>Balirong National High School&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2013&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Service Awardee</h5></p>
+<p><h5>Cebu Technological University</h5></p>
+<div>
+<h1><b>Biography</b></h1>
+</div>
+<p><h5>&nbsp;&nbsp;&nbsp;&nbsp;My name is Melfer P. Algabre, currently living in City of Naga Cebu with my younger twin sisters.</h5></p> 
+<p><h5>Taking up Bachelor of Science in Information and Communication Technology(BSICT) at Cebu Technological University(CTU) Naga-Extension Campus.</h5></p>
+<p><h5>I ride "sikad" everyday from home to school and the other way.</h5></p>
+</body>
+</html>
